b445e8ffcad6b07b260de55496e107bae9784928
Nataliya Trushina <nitrushina@hotmail.com>
2023-02-15 15:05:31 +0100

    Learning *args and **kwargs use. Homework on args and formatting.

--- W3T3_Uebung_args_Wertetabelle_Nataliya.py
+++ W3T3_Uebung_args_Wertetabelle_Nataliya.py
@@ -10,18 +10,18 @@ def wertetabelle(fkt, von, bis):
     max_len_left = 0
     max_len_right = 0
     for i in range(von, bis + 1):
-        if len(str(i)) > max_len_left:
-            max_len_left = len(str(i)) + 2
-        if len(str(fkt(i))) > max_len_right:
-            max_len_right = len(str(fkt(i))) + 2
-
+        if len(str(i)) >= max_len_left:
+            max_len_left = len(str(i)) + 1
+        if len(str(fkt(i))) >= max_len_right:
+            max_len_right = len(str(fkt(i))) + 1
+        print(len(str(fkt(i))))
     x, y, dash = "x", "y", "-"
     print(f"{x:^{max_len_left}}|{y:^{max_len_right}}")
     print(f"{dash * max_len_left}+{dash * max_len_right}")
     for i in range(von, bis + 1):
-        print(f"{i:^{max_len_left}}|{fkt(i):^{max_len_right}}")
+        print(f"{i:{max_len_left}}|{fkt(i):{max_len_right}}")
 
 
 if __name__ == "__main__":
-    wertetabelle(quadrat, 30000, 30005)
+    wertetabelle(quadrat, 2, 20)
     wertetabelle(mult, 1, 5)
