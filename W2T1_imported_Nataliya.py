621c7d886023959e135b2eccfe40eb258cec8c44
Nataliya Trushina <nitrushina@hotmail.com>
2023-07-11 16:26:25 +0200

    App code update

--- W2T1_imported_Nataliya.py
+++
@@ -1,7 +0,0 @@
-def quadrat(x):
-    return x**2
-
-print("Das ist innere Programm:", __name__)
-
-if (__name__ == "__main__"):
-    print("Diese Zeile wird gelaufen wenn zugegriffen aus main:", __name__)
