da2bbd0da530453451158577238e21e706382145
Nataliya Trushina <nitrushina@hotmail.com>
2023-07-13 21:16:33 +0200

    Save plots and modify coordinates upon clicking

--- BioSketchMapper_MainApp.py
+++ BioSketchMapper_MainApp.py
@@ -13,32 +13,6 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 import matplotlib.image as mpimg
 from matplotlib.backend_bases import MouseButton
 
-def create_dataframe_from_collection(collection):
-    genes = []
-    aliases = []
-    xs = []
-    ys = []
-    expressions = []
-    for instance in collection:
-        genes.append(instance.name)
-        aliases.append(instance.aliases)
-        xs.append(instance.x)
-        ys.append(instance.y)
-        expressions.append(instance.expression)
-
-    # create a dictionary
-    dict_for_pd = {
-        'Genes': genes,
-        'Aliases': aliases,
-        'x': xs,
-        'y': ys,
-        'Expression': expressions
-    }
-
-    # create a data frame
-    dataframe = pd.DataFrame(dict_for_pd)
-    return dataframe
-
 
 class MainApplication(tk.Tk):
 
@@ -76,6 +50,7 @@ class YourControllerClass(tk.Tk):  # or whatever your controller class is
         self.sketch_file = None
         self.sketch_file_for_mapping = None
         self.instances = None
+        self.values = None
 
 
 class StartPage(tk.Frame):
@@ -105,26 +80,6 @@ class StartPage(tk.Frame):
         notebook.pack(expand=1, fill='both')
 
 
-def close_on_click(event):
-    plt.close()
-
-
-def find_coords(figure_file):
-    image = plt.imread(figure_file)
-    fig, ax = plt.subplots()
-    ax.imshow(image, cmap='gray')
-    klicker = clicker(ax, ['genes'], markers=['o'])
-    # close the window automatically after the first click
-    fig.canvas.mpl_connect('button_press_event', close_on_click)
-    plt.show()
-
-    # Get x and y values for approximate label placement
-    for coords in klicker.get_positions().values():
-        for x, y in coords:
-            # print(f"{x:.2f},{y:.2f}")
-            x = round(x / 10) * 10
-            y = round(y / 10) * 10
-            return x, y
 
 
 class MapCoordinates(tk.Frame):
@@ -197,8 +152,8 @@ class MapCoordinates(tk.Frame):
 
                         # Reset current instance index to 0 and make the button reappear
                         self.current_instance_index = 0
+                        self.find_coords_button.config(text="Save Results", command=self.save_results)
                         self.find_coords_button.pack()
-                        # break
 
                     # redraw the canvas
                     self.canvas.draw()
@@ -210,12 +165,88 @@ class MapCoordinates(tk.Frame):
         self.cid = self.figure.canvas.mpl_connect('button_press_event', onclick)
         self.canvas.draw()
 
+    def save_results(self):
+        instances = self.controller.instances
         df = create_dataframe_from_collection(instances)
-        # plot_coords(df)
-
+        
         # Save the collection to a file using pickle
         with open("c:/Users/NITru/OneDrive/Documents/PhD_work/GitHub/BioSketchMapper/coords_collection_output.pkl", "wb") as f:
             pickle.dump(instances, f)
+
+        # Change the button text and command back to original
+        self.find_coords_button.config(text="Find coordinates", command=self.find_coordinates)
+        
+
+def create_dataframe_from_collection(collection):
+    genes = []
+    aliases = []
+    xs = []
+    ys = []
+    expressions = []
+    for instance in collection:
+        genes.append(instance.name)
+        aliases.append(instance.aliases)
+        xs.append(instance.x)
+        ys.append(instance.y)
+        expressions.append(instance.expression)
+
+    # create a dictionary
+    dict_for_pd = {
+        'Genes': genes,
+        'Aliases': aliases,
+        'x': xs,
+        'y': ys,
+        'Expression': expressions
+    }
+
+    # create a data frame
+    dataframe = pd.DataFrame(dict_for_pd)
+    return dataframe
+
+
+def label_point(x, y, val, color, ax):
+    a = pd.concat({'x': x + 15, 'y': y + 10, 'val': val}, axis=1)
+    for i, point in a.iterrows():
+        ax.text(point['x'], point['y'], str(point['val']), color=color)
+
+
+def plot_coords(data_inp, sketch_file, ax, fig, highlight_gene=None, colorbar_owner=None):
+    nan_data = data_inp[data_inp['Expression'].isnull()]
+    data = data_inp[data_inp['Expression'].notnull()]
+
+    img = plt.imread(sketch_file)
+
+    ax.clear()  # clear the axes
+    ax.imshow(img)  # , extent=[0, 500, 0, 500]
+
+    # Plot data points
+    cmap = plt.get_cmap("RdYlGn")
+    scatter = ax.scatter(data['x'], data['y'], s=100,
+              c=data['Expression'],
+              edgecolors='black', linewidths=1,
+              cmap=cmap)
+    
+    # Add color bar
+    if colorbar_owner is not None:
+        colorbar_owner.colorbar = fig.colorbar(scatter)
+
+    # Plot NaN data points
+    ax.scatter(nan_data['x'], nan_data['y'], s=100,
+                  fc=(0.3, 0.3, 0.3, 0.7),
+                  edgecolors='black', linewidths=1)
+
+    # Add labels to data points
+    label_point(data.x, data.y, data.Genes, color="black", ax=ax)
+    label_point(nan_data.x, nan_data.y, nan_data.Genes, color="black", ax=ax)
+
+    # Highlight the specified gene in red
+    if highlight_gene:
+        gene_data = data_inp[data_inp['Genes'] == highlight_gene]
+        ax.scatter(gene_data['x'], gene_data['y'], s=100,
+                       fc='red', ec='black', linewidths=1)
+        label_point(gene_data.x, gene_data.y, gene_data.Genes, color='red', ax=ax)
+
+    fig.canvas.draw()  # update the canvas
 
 
 class ShowValueColor(tk.Frame):
@@ -223,27 +254,151 @@ class ShowValueColor(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.figure = plt.Figure()
+        self.ax = self.figure.add_subplot(111)
+        self.canvas = FigureCanvasTkAgg(self.figure, self)
+        self.canvas.get_tk_widget().pack()
 
         self.map_values_button = tk.Button(self, text="Map values", command=self.map_values)
         self.map_values_button.pack()
 
+        self.save_button = tk.Button(self, text="Save Plot", command=self.save_plot)
+        self.save_button.pack()
+
+        self.colorbar = None
+
+        self.save_count = 1  # This will keep track of how many times you've saved the figure
+
+    def save_plot(self):
+        default_filename = f"Figure{self.save_count:03}.svg"
+        filename = filedialog.asksaveasfilename(defaultextension=".svg", initialfile=default_filename)
+        if filename:  # To ensure the dialog wasn't cancelled
+            self.figure.savefig(filename, format='svg')
+            self.save_count += 1
+
+    def find_gene_IDs_in_instances(self, input_values, insts):
+        for k, v in input_values.items():
+            for instance in insts:
+                if instance.aliases is not None and isinstance(instance.aliases, list):
+                    if k.upper() in {x.upper() for x in instance.aliases}:
+                        instance.expression = v
+
     def map_values(self):
-        # Functionality for mapping values goes here
-        pass
+        print(self.controller.values) 
+        file_path = "c:/Users/NITru/OneDrive/Documents/PhD_work/GitHub/BioSketchMapper/coords_collection_output.pkl"
+        
+        if os.path.exists(file_path):
+            # Load the instances from the file
+            with open(file_path, "rb") as f:
+                instances = pickle.load(f)
+
+            # Map instance values
+            self.find_gene_IDs_in_instances(self.controller.values, instances)
+
+            # Convert instances to dataframe
+            df = create_dataframe_from_collection(instances)
+
+            # Clear color bar
+            if self.colorbar is not None:
+                self.colorbar.remove()
+                self.colorbar = None
+            
+            # Plot instances on the sketch file
+            plot_coords(df, sketch_file=self.controller.sketch_file, ax=self.ax, fig=self.figure, colorbar_owner=self)
+
+        else:
+            # Show a message telling the user to go to the MapCoordinates tab
+            tk.messagebox.showinfo("Information", "No coordinates file found. Please go to the MapCoordinates tab and generate it.")
 
 
 class ModifyCoordinates(tk.Frame):
-    # Corresponds to "Modify coordinates"
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        self.modify_coords_button = tk.Button(self, text="Modify coordinates", command=self.select_object_window)
+        self.figure = plt.Figure()
+        self.ax = self.figure.add_subplot(111)
+
+        self.canvas = FigureCanvasTkAgg(self.figure, self)
+        self.canvas.get_tk_widget().pack()
+
+        self.modify_coords_button = tk.Button(self, text="Modify coordinates", command=self.modify_coordinates)
         self.modify_coords_button.pack()
 
-    def select_object_window(self):
-        # Functionality for modifying coordinates goes here
-        pass
+        self.save_and_exit_button = tk.Button(self, text="Save and Exit", command=self.save_and_exit)
+        self.save_and_exit_button.pack()
+
+        self.selected_instance = None
+        self.cid = None
+        self.instances = None  # New instance variable to store instances
+
+    def modify_coordinates(self):
+        self.modify_coords_button.pack_forget()  # Hide the button
+        
+        # Add a label for instructions
+        self.instructions = tk.Label(self, text="Click on the instance you want to change.")
+        self.instructions.pack()
+
+        file_path = "c:/Users/NITru/OneDrive/Documents/PhD_work/GitHub/BioSketchMapper/coords_collection_output.pkl"
+
+        if os.path.exists(file_path):
+            # Load the instances from the file
+            with open(file_path, "rb") as f:
+                self.instances = pickle.load(f)  # Use the new instance variable
+
+        df = create_dataframe_from_collection(self.instances)  # Convert instances to dataframe
+
+        # Clear current plot and show instances
+        self.ax.clear()
+        plot_coords(df, sketch_file=self.controller.sketch_file, ax=self.ax, fig=self.figure)
+
+        # Capture click events
+        def onclick(event):
+            if self.selected_instance is None:
+                self.instructions.config(text="Click on the new position.")
+            else:
+                self.instructions.config(text="Click on the instance you want to change.")
+
+            if event.button is MouseButton.LEFT and event.xdata is not None and event.ydata is not None:
+                x = round(event.xdata / 10) * 10
+                y = round(event.ydata / 10) * 10
+
+                if self.selected_instance is None:  # If no instance is currently selected, try to select one
+                    for instance in self.instances:
+                        if abs(instance.x - x) < 10 and abs(instance.y - y) < 10:  # If the click is close to an instance
+                            self.selected_instance = instance  # Select the instance
+                            break
+                else:  # If an instance is currently selected, move it to the new coordinates
+                    self.selected_instance.x = x
+                    self.selected_instance.y = y
+
+                    # Unselect the instance
+                    self.selected_instance = None
+
+                    # Redraw the plot with updated coordinates
+                    self.ax.clear()
+                    df = create_dataframe_from_collection(self.instances)
+                    plot_coords(df, sketch_file=self.controller.sketch_file, ax=self.ax, fig=self.figure)
+                    self.canvas.draw()
+
+        # Disconnect previous click listener if it exists
+        if self.cid is not None:
+            self.figure.canvas.mpl_disconnect(self.cid)
+
+        # Connect the click listener
+        self.cid = self.figure.canvas.mpl_connect('button_press_event', onclick)
+        self.canvas.draw()
+
+    def save_and_exit(self):
+        file_path = "c:/Users/NITru/OneDrive/Documents/PhD_work/GitHub/BioSketchMapper/coords_collection_output.pkl"
+
+        # Save the instances back to the file
+        with open(file_path, "wb") as f:
+            pickle.dump(self.instances, f)  # Use the new instance variable
+
+        # Show the button again and disconnect the click listener
+        self.modify_coords_button.pack()
+        self.figure.canvas.mpl_disconnect(self.cid)
 
 
 class OpenSketch(tk.Frame):
@@ -396,8 +551,6 @@ class OpenValues(tk.Frame):
             rows = (line.strip('\n').split('\t') for line in fin)
             input_values = {row[0]: float(row[1]) for row in rows}
         return input_values
-    
-    
     
 
 class value_on_figure():
